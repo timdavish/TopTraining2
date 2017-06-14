551b57df1152901b49bd73ea8fe7f83b4c0462cb
Angular CLI <angular-cli@angular.io>
2017-06-14 10:06:27 -0700

    chore: initial commit from @angular/cli

---
+++ e2e/app.e2e-spec.ts
@@ -0,0 +1,14 @@
+import { TopTraining2Page } from './app.po';
+
+describe('top-training2 App', () => {
+  let page: TopTraining2Page;
+
+  beforeEach(() => {
+    page = new TopTraining2Page();
+  });
+
+  it('should display welcome message', () => {
+    page.navigateTo();
+    expect(page.getParagraphText()).toEqual('Welcome to app!!');
+  });
+});
