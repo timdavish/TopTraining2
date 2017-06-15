713c2e5ab9b53f91fb3a58c4a8ec512afad9dd11
Tim Davis <timdavish@gmail.com>
2017-06-14 23:01:58 -0700

    Update header and footer

--- client/src/app/shared/layout/header.component.ts
+++ client/src/app/shared/layout/header.component.ts
@@ -1,13 +1,32 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+import { UserService } from '../services';
+import { User } from '../models';
 
 @Component({
 	selector: 'layout-header',
-	templateUrl: './header.component.html'
+	templateUrl: './header.component.html',
+	styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
-	constructor() { }
+	constructor(
+		private router: Router,
+		private userService: UserService
+	) {}
 
-	ngOnInit() {
+	currentUser: User;
 
+	ngOnInit(): void {
+		this.userService.currentUser.subscribe(
+			userData => {
+				this.currentUser = userData;
+			}
+		);
+	}
+
+	public logout(): void {
+		this.userService.purgeAuth();
+		this.router.navigateByUrl('/');
 	}
 }
