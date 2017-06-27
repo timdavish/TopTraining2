f3fbf1092600776559b5c1c9cc01172cca667ed5
Tim Davis <timdavish@gmail.com>
2017-06-26 20:05:23 -0700

    Add Sport Mongoose model

---
+++ server/models/Sport.js
@@ -0,0 +1,28 @@
+
+// Module dependencies
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const ObjectId = mongoose.Schema.Types.ObjectId;
+
+// Schema
+const SportSchemaOptions = { timestamps: true };
+
+const SportSchema = new Schema({
+    sport: { type: String, required: [true, "can't be blank"], unique: true },
+	trainers: [{ type: ObjectId, ref: 'User' }]
+}, SportSchemaOptions);
+
+SportSchema.methods.addTrainer(id) {
+	if (this.trainers.indexOf(id) === -1) {
+		this.trainers.push(id);
+	}
+	return this.save();
+};
+
+SportSchema.methods.removeTrainer(id) {
+	this.trainers.remove(id);
+	return this.save();
+};
+
+// Set mongoose model
+mongoose.model('Sport', SportSchema);
