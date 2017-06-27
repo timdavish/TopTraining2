03776abd91d56f1b26469ef49badee15f58137c4
Tim Davis <timdavish@gmail.com>
2017-06-26 20:06:39 -0700

    General server setup

--- server/server.js
+++ server/server.js
@@ -1,13 +1,15 @@
-var http = require('http'),
-    path = require('path'),
-    methods = require('methods'),
-    express = require('express'),
-    bodyParser = require('body-parser'),
-    session = require('express-session'),
-    cors = require('cors'),
-    passport = require('passport'),
-    errorhandler = require('errorhandler'),
-    mongoose = require('mongoose');
+
+// Dependencies
+var express = require('express');
+var http = require('http');
+var path = require('path');
+var methods = require('methods');
+var bodyParser = require('body-parser');
+var session = require('express-session');
+var cors = require('cors');
+var passport = require('passport');
+var errorhandler = require('errorhandler');
+var mongoose = require('mongoose');
 
 var isProduction = process.env.NODE_ENV === 'production';
 
@@ -26,54 +28,57 @@ app.use(express.static(__dirname + '/public'));
 
 app.use(session({ secret: 'conduit', cookie: { maxAge: 60000 }, resave: false, saveUninitialized: false  }));
 
+// Use errorhandler in development
 if (!isProduction) {
-  app.use(errorhandler());
+	app.use(errorhandler());
 }
 
+// Connect to our mongodb
 if (isProduction) {
-  mongoose.connect(process.env.MONGODB_URI);
+	mongoose.connect(process.env.MONGODB_URI);
 } else {
-  mongoose.connect('mongodb://localhost/TT');
-  mongoose.set('debug', true);
+	mongoose.connect('mongodb://localhost/TT');
+	mongoose.set('debug', true);
 }
 
+// require('./config/passport');
 // require('./models/User');
 // require('./models/Article');
 // require('./models/Comment');
-// require('./config/passport');
 
 app.use(require('./routes'));
 
-/// catch 404 and forward to error handler
+// Catch 404 and forward to a error handler
 app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+	var err = new Error('Not Found');
+	err.status = 404;
+	next(err);
 });
 
-/// error handlers
+// Error handlers
+if (isProduction) {
+	// Production error handler (no stacktraces leaked to user)
+	app.use(function(err, req, res, next) {
+		res.status(err.status || 500);
+		res.json({'errors': {
+			message: err.message,
+			error: {}
+		}});
+	});
+} else {
+	// Development error handler (prints stacktrace)
+	app.use(function(err, req, res, next) {
+		console.log(err.stack);
 
-// development error handler
-// will print stacktrace
-if (!isProduction) {
-  app.use(function(err, req, res, next) {
-    console.log(err.stack);
-
-    res.status(err.status || 500);
-
-    res.json({'errors': {
-      message: err.message,
-      error: err
-    }});
-  });
+		res.status(err.status || 500);
+		res.json({'errors': {
+			message: err.message,
+			error: err
+		}});
+	});
 }
 
-// production error handler
-// no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.json({'errors': {
-    message: err.message,
-    error: {}
-  }});
+// Start server
+var server = app.listen(process.env.PORT || 3000, function() {
+	console.log('Listening on port ' + server.address().port);
 });
