ce5a02bdcb71a00bf9a4f1f423f9aa7e0de62323
Tim Davis <timdavish@gmail.com>
2017-07-07 13:13:08 -0700

    Set up ApplyComponent Closes #14

--- client/src/app/apply/apply.component.ts
+++ client/src/app/apply/apply.component.ts
@@ -1,12 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
 import { Router } from '@angular/router';
+import { Subject } from 'rxjs/Subject';
+import 'rxjs/add/operator/takeUntil';
+
+import { UserService } from '../shared';
 
 @Component({
 	selector: 'apply-page',
-	templateUrl: './apply.component.html'
+	templateUrl: './apply.component.html',
+	styleUrls: ['./apply.component.css']
 })
-export class ApplyComponent {
+export class ApplyComponent implements OnDestroy {
+	private ngUnsubscribe: Subject<void> = new Subject<void>();
+
 	constructor(
-		private router: Router
+		private router: Router,
+		private userService: UserService
 	) {}
+
+	ngOnDestroy(): void {
+		// Clean up subscriptions
+		this.ngUnsubscribe.next();
+		this.ngUnsubscribe.complete();
+	}
+
+	public cancelForm(): void {
+		const user = this.userService.getCurrentUser();
+		this.userService.delete(user)
+			.takeUntil(this.ngUnsubscribe)
+			.subscribe(
+				data => {
+					this.userService.purgeAuth();
+					this.router.navigateByUrl('/register');
+				},
+				err => {}
+			);
+	}
 }
