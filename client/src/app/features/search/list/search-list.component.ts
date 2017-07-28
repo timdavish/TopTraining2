bcaccd138116244958d0241127d96bff5ac9ea3f
Tim Davis <timdavish@gmail.com>
2017-07-28 15:18:55 -0700

    fix(SearchList): Works with new SearchResolver
    - PagerService is used correctly to page the trainer results
    - Added filter capability, sort capability
    - So far sort only works if you reload the page.  Can't figure out how
    to get the sort pipe to refire when the sort property changes

--- client/src/app/features/search/list/search-list.component.ts
+++ client/src/app/features/search/list/search-list.component.ts
@@ -1,51 +1,98 @@
-import { Component, Input, OnDestroy } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 import { Subject } from 'rxjs/Subject';
 
 import { Pager, PagerService } from 'app/core/services'
 import { SearchService } from '../shared/search.service';
-import { Errors, SearchFilters, SearchQuery, TrainerProfile } from 'app/shared/models';
+import { Errors, SearchFilters, searchFiltersState, SearchQuery, TrainerProfile } from 'app/shared/models';
 
 @Component({
 	selector: 'search-list',
 	templateUrl: './search-list.component.html',
 	styleUrls: ['./search-list.component.css']
 })
-export class SearchListComponent implements OnDestroy {
-	@Input() filters: SearchFilters;
-	@Input() query: SearchQuery;
-	@Input() set profiles(profiles: TrainerProfile[]) {
-		if (profiles && profiles.length) {
-			this.profileList = profiles;
-			this.setPage(1);
-		}
-	};
+export class SearchListComponent implements OnChanges, OnInit {
+	@Input() profiles: TrainerProfile[];
+	@Input() count: number;
+	@Input() sport: string;
+	@Input() location: string;
 
 	errors: Errors = new Errors();
+	filters: SearchFilters = searchFiltersState;
+	sort: string;
 	limit: number = 5;
-	profileList: TrainerProfile[];
 	pageSize: number = 0;
 	pager: Pager = new Pager();
-	pagedProfiles: TrainerProfile[];
+	get pagedProfiles() { return this._pagedProfiles; }
 
-	private ngUnsubscribe: Subject<void> = new Subject<void>();
+	private _pagedProfiles: TrainerProfile[] = [];
 
 	constructor(
 		private pagerService: PagerService,
+		private route: ActivatedRoute,
+		private router: Router,
 		private searchService: SearchService
 	) {}
 
-	ngOnDestroy(): void {
-		// Clean up subscriptions
-		this.ngUnsubscribe.next();
-		this.ngUnsubscribe.complete();
+	ngOnInit(): void {
+		// Subscribe to route params
+		this.route.queryParams.subscribe(params => {
+			console.log('queryParamsChange', params);
+			Object.assign(this.filters, params);
+			this.setSort(this.filters.sort);
+			const newProfiles = [...this.profiles];
+			this.profiles = newProfiles;
+		});
+	}
+
+	ngOnChanges(changes: SimpleChanges): void {
+		console.log(changes);
+		if (changes['profiles']) {
+			this.profiles = changes['profiles'].currentValue;
+
+			if (changes['count']) {
+				this.count = changes['count'].currentValue;
+			}
+
+			this.setPage(1);
+		}
+
+		if (changes['location']) {
+			this.location = changes['location'].currentValue;
+		}
+
+		if (changes['sport']) {
+			this.sport = changes['sport'].currentValue;
+		}
 	}
 
 	setPage(page: number): void {
-		if (page < 1 || page > this.pager.totalPages) {
+		let totalPages = Math.ceil(this.count / this.limit);
+
+		if (page < 1 || (this.count > 0 && page > totalPages)) {
 			return;
 		}
 
-		this.pager = this.pagerService.getPager(this.profileList.length, page, this.limit);
-		this.pagedProfiles = this.profileList.slice(this.pager.startIndex, this.pager.endIndex);
+		this.pager = this.pagerService.getPager(this.count, page, totalPages, this.limit);
+		this._pagedProfiles = this.profiles.slice(this.pager.startIndex, this.pager.endIndex);
+	}
+
+	onSortChange() {
+		this.router.navigate([], {
+			queryParams: this.filters,
+			relativeTo: this.route
+		});
+	}
+
+	private setSort(sort: string): void {
+		console.log('this.sort before:', this.sort);
+		if (sort === 'rating') {
+			this.sort = '-trainer.rating.average';
+		} else if (sort === 'price') {
+			this.sort = 'packages.0.price';
+		} else if (sort === 'distance') {
+			this.sort = 'dist.calculated';
+		}
+		console.log('this.sort has LITERALLY CHANGED, SO CAN THE PIPE REFIRE?', this.sort);
 	}
 }
